d31d423eff8e459065c9013bef752df1ef95c5f5
Bryan Soria Méndez <bryan.soria@agrosoft.com.ec>
2025-11-05 22:51:44 -0500

    🎉 Initial commit

---
+++ src/test/java/com/hireflow/HireFlow/HireFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hireflow.HireFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
